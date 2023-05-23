c0e4fcf88c2e02dfe96398f0f0f061b35d7447c7
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-05-23 12:59:28 +0300

    update

--- src/js/01-1-creation.js
+++ src/js/01-1-creation.js
@@ -5,6 +5,19 @@
  * - Как отличить объект от области видимости
  */
 
+// number string boolean undefined null
+
+// array function
+
+// const arr = [10, 20, 30, 40];
+
+// const user = {
+//   name: 'Buratino',
+//   phoneNumber: '+380......',
+//   birthday: '20.08.1980',
+//   age: 20,
+// };
+
 /* 
 Створення об'єкта
 Вкладені властивості
@@ -19,10 +32,98 @@
 Доступ до властивостей об'єкта в методах
 */
 
+// const user = {
+//   name: 'Buratino',
+//   age: 20,
+// };
+
+// user.name = 'New Name';
+
+// user.job = 'Test';
+// user.location = {};
+
+// console.log(user);
+// console.log(user1.location.position.x);
+// console.log(user['location']['position']['log']);
+
+// ========================
+
+// const username = 'Roman';
+// const age = 20;
+
+// const user = {
+//     username: username,
+//     age: age,
+// };
+
+/* const key = 'test';
+
+const user = {
+  key,
+  [key]: username,
+
+    test: 'Hello',
+    test: 'World',
+    test: 'empty',
+    name: ''
+};
+
+console.log(user); */
+// ================================
+/* 
+const user = {
+  name: 'Buratino',
+
+  sum(x, y) {
+    console.log(`${x} + ${y} = ${x + y}`);
+  },
+
+  showName() {
+    console.log(this.name);
+    console.log(this.name);
+    console.log(this.name);
+  },
+};
+
+const user1 = {
+  name: 'Papa Karlo',
+
+  sum(x, y) {
+    console.log(`${x} + ${y} = ${x + y}`);
+  },
+
+  showName() {
+    this.testProps = '123';
+    console.log(this.name);
+    console.log(this.name);
+  },
+};
+
+user1.newProps = '123';
+
+user.showName();
+
+console.log(user1); */
 /*
  * Ссылочный тип {} === {}
  */
 
+const a = { name: 'Roman' };
+const b = { name: 'Roman' };
+
+const x = a;
+
+console.log(a === x);
+
 /*
  * Массивы и функции это объекты
  */
+
+const arr = [1, 2, 3];
+
+arr[arr.length] = 123;
+console.log(arr);
+
+// function myFun() {
+//   console.log(1123);
+// }
--- src/js/01-3-iteration.js
+++ src/js/01-3-iteration.js
@@ -2,21 +2,66 @@
  * Перебор через for...in и Object.keys|values|entries
  */
 
-let feed = {
+let father = {
   bad: 3,
   good: 5,
   neutral: 10,
 };
 
-let feedback = Object.create(feed);
-feedback.test = 'myTest';
+const child = Object.create(father);
 
-// for (let key of Object.keys(feedback)) {
-//   console.log(feedback[key]);
+child.newName = 'child Name';
+child.phone = '123';
+child.age = '234';
+
+console.log(child);
+
+for (let key in child) {
+  if (child.hasOwnProperty(key)) console.log(key, child[key]);
+}
+
+const user = {
+  name: [],
+  age: 'awd2',
+  phone: 'awd3',
+  patronymic: 'awd4',
+  location: 'awd5',
+  isActive: 'awd6',
+  status: 'awd7',
+  emptyKey: undefined,
+  showName() {
+    console.log(234);
+  },
+};
+
+// console.log(user);
+
+const result1 = Object.keys(user);
+const result2 = Object.values(user);
+const result3 = Object.entries(user);
+
+console.log(result1);
+// for (let key of result1) {
+//   console.log(key, user[key]);
+// }
+
+// for (let key of result2) {
+//   console.log(key);
+// }
+
+// for (let arrPair of result3) {
+//   console.log(arrPair);
+//   const key = arrPair[0];
+//   const value = arrPair[1];
 // }
 
 // for (let key in feedback) {
 //   if (feedback.hasOwnProperty(key)) console.log(feedback[key]);
 // }
 
-let totalFeedback = 0;
+// let totalFeedback = 0;
+
+const user1 = {
+  name: 'Yarik',
+};
+console.log(user1.hasOwnProperty('phoneNumber'));
