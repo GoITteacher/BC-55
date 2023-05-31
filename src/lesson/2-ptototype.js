a48871326d4ea7e3e70b5a322e5619bc7db37b01
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-05-31 13:17:11 +0300

    update

--- src/lesson/2-ptototype.js
+++ src/lesson/2-ptototype.js
@@ -1,3 +1,23 @@
 //Прототипне наслідування
 
 // Object;
+
+// const num = 10;
+// const str = 'Hello';
+
+const objA = {
+  name: 'ObjA',
+  age: 20,
+  getName() {
+    console.log(this.name);
+  },
+};
+
+const objB = Object.create(objA);
+objB.city = '345';
+objB.country = '123';
+
+// console.log(objB.__proto__);
+// for (const key in objB) {
+//   console.log(key);
+// }
