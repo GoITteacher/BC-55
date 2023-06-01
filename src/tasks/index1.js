e284a4b205917693df95254b4e01c115e0fc70a7
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-06-01 10:28:15 +0300

    update

--- src/tasks/index1.js
+++ src/tasks/index1.js
@@ -8,3 +8,54 @@
 роботу класів, створивши об'єкти "Прямокутник" і "Квадрат" і використовуючи їх
 методи для обчислення площі і периметра.
 */
+
+class Figure {
+  #width;
+  #height;
+
+  constructor(width, height) {
+    this.#width = width;
+    this.#height = height;
+  }
+
+  getArea() {
+    return this.#height * this.#width;
+  }
+  getPerimeter() {
+    return 2 * this.#height + 2 * this.#width;
+  }
+
+  get width() {
+    return this.#width;
+  }
+  get height() {
+    return this.#height;
+  }
+
+  showInfo() {
+    console.log('--------------');
+    console.log('Area:', this.getArea());
+    console.log('Per:', this.getPerimeter());
+    console.log('Width:', this.width);
+    console.log('Height', this.height);
+    console.log('--------------');
+  }
+}
+
+class Square extends Figure {
+  constructor(lenA) {
+    super(lenA, lenA);
+  }
+}
+
+class Rectangle extends Figure {
+  constructor(phone1, phone2) {
+    super(phone1, phone2);
+  }
+}
+
+const square = new Square(10);
+const rectangleA = new Rectangle(10, 20);
+
+square.showInfo();
+rectangleA.showInfo();
